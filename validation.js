09697999b241e0b366f2e50b6d373a20efc4787c
Loic <72917658+Hirwacardinalloic@users.noreply.github.com>
2023-01-07 14:46:22 +0200

    Added validation.js

--- validation.js
+++ validation.js
@@ -1,7 +1,7 @@
 const formbutton = document.getElementById('form-button');
 const Email = document.getElementById('email');
 const Errormessage = document.getElementById('form-error');
-Errormessage.textContent = 'Message not sent! Please use only small letters. Thank you.';
+Errormessage.textContent = 'Message not sent! Please use letters only. Thank you.';
 
 function validationForm(e) {
   if (Email.value !== Email.value.toLowerCase()) {
