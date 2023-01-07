140ffe7958187f931cc65c6f4bc6153b7f32a4c9
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-01-07 12:57:17 +0000

    Change error message in validation.js file and styling on error

--- validation.js
+++ validation.js
@@ -1,7 +1,7 @@
 const formbutton = document.getElementById('form-button');
 const Email = document.getElementById('email');
 const Errormessage = document.getElementById('form-error');
-Errormessage.textContent = 'Message not sent! Please use letters only. Thank you.';
+Errormessage.textContent = 'Message not sent! Please use lower case letters only for the email. Thank you.';
 
 function validationForm(e) {
   if (Email.value !== Email.value.toLowerCase()) {
